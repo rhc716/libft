71fd57a7fa6b126f2c69e70d97f73daab267ad7c
melon <jack716@naver.com>
2020-09-29 00:34:51 +0900

    add part1 functions

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hroh <hroh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 22:27:40 by hroh              #+#    #+#             */
+/*   Updated: 2020/09/29 00:14:44 by hroh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_lower(int c);
+int	ft_toupper(int c);
+int	ft_isspace(int c);
+int	ft_atoi(const char *str);
+
+#endif
